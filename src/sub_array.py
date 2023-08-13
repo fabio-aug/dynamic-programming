82a73c8977232615867832c4b77f878c08ff7778
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-08-13 11:41:04 -0300

    Change sub array code

--- src/sub_array.py
+++ src/sub_array.py
@@ -1,19 +1,51 @@
+import random
+from datetime import datetime
 
-def calculate(num):
+array_compare = []
+array_moves = []
+
+
+def calculate(num, id):
+    global array_compare, array_moves
+
+    movements = 0
+    comparisons = 0
+    time = datetime.now()
+
     n = len(num)
     best = [0] * n
-
+    next = [None] * n
     for i in range(n - 1, -1, -1):
+        movements += 1
         best[i] = 1
         for j in range(i + 1, n):
+            comparisons += 1
             if num[j] > num[i] and 1 + best[j] > best[i]:
+                movements += 2
                 best[i] = 1 + best[j]
+                next[i] = j + 1
 
-    print(best)
-    print(max(best))
+    print(f'\nResultado ({id}) ------------------')
+    print(f'Tempo: {datetime.now() - time}')
+    print(f'Comparações: {comparisons}')
+    print(f'Movimentações: {movements}')
+    print(f'Num: {num}')
+    print(f'Best: {best}')
+    print(f'Next: {next}')
+
+    array_compare.append(comparisons)
+    array_moves.append(movements)
 
 
 if __name__ == '__main__':
-    array = [7, 6, 10, 3, 4, 1, 8, 9, 5, 2, 11, 14, 15, 46, 13, 10]
-    calculate(array)
+    array_x = []
+    for i in range(100):
+        array_x.append(i + 1)
+        index = i + 1
+        array = [random.randint(index, 1000) for i in range(index)]
+        calculate(array, index)
 
+    print(f'\n\n-----------------------------------')
+    print(f'array_x = {array_x}')
+    print(f'array_compare = {array_compare}')
+    print(f'array_moves = {array_moves}')
